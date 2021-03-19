09204d7baf5283632bbda3daecfbd5525951eca2
youssef-ladhari <deathwastaken@yahoo.com>
2021-03-19 13:59:35 +0100

    first commit

---
+++ auth/src/models/user.ts
@@ -0,0 +1,92 @@
+import mongoose from "mongoose";
+import { Password } from "../services/password";
+
+// An interface that describes the properties
+// that are requried to create a new User
+interface UserAttrs {
+  email: string;
+  password: string;
+  name: string;
+  lastname: string;
+  sexe: string;
+  age: string;
+  adresse: string;
+}
+
+// An interface that describes the properties
+// that a User Model has
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
+// An interface that describes the properties
+// that a User Document has
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
+  name: string;
+  lastname: string;
+  sexe: string;
+  age: string;
+  adresse: string;
+}
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: false,
+    },
+    lastname: {
+      type: String,
+      required: false,
+    },
+    sexe: {
+      type: String,
+      required: false,
+    },
+    age: {
+      type: String,
+      required: false,
+    },
+    adress: {
+      type: String,
+      required: false,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.password;
+        delete ret.__v;
+      },
+    },
+  }
+);
+
+userSchema.pre("save", async function (done) {
+  if (this.isModified("password")) {
+    const hashed = await Password.toHash(this.get("password"));
+    this.set("password", hashed);
+  }
+  done();
+});
+
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
+
+const User = mongoose.model<UserDoc, UserModel>("User", userSchema);
+
+export { User };
